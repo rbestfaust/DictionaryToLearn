e1ad92283b65023fcbc00a4351fb7393b5b9e577
Roman <rbestfaust@gmail.com>
2021-04-20 13:44:54 +0300

    Full refactoring .Added Checker() and etc

--- dictionaryForms/Form1.Designer.cs
+++ dictionaryForms/Form1.Designer.cs
@@ -36,6 +36,7 @@ namespace dictionaryForms
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -55,7 +56,7 @@ namespace dictionaryForms
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(167, 32);
             this.textBox1.TabIndex = 3;
-            
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -64,6 +65,7 @@ namespace dictionaryForms
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(170, 32);
             this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
@@ -103,11 +105,22 @@ namespace dictionaryForms
             this.label4.TabIndex = 8;
             this.label4.Text = "label4";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(566, 272);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 53);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Practice";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -131,6 +144,7 @@ namespace dictionaryForms
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- dictionaryForms/Form1.cs
+++ dictionaryForms/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Media;
 
 namespace dictionaryForms
 {
@@ -16,63 +17,130 @@ namespace dictionaryForms
     public partial class Form1 : Form
     {
 
+       static public  int Counter()
+        {
+            int count = 0;
+            count = File.ReadAllLines(pathEng).Length;
 
-        string pathEng = @"C:\Users\Roman\Desktop\engWords\EngWords.txt";
+
+            return count;
+            
+
+        }
+
+       static string pathEng = @"C:\Users\Roman\Desktop\engWords\EngWords.txt";
         string pathRus = @"C:\Users\Roman\Desktop\engWords\RusWords.txt";
+        
+        int commonCount = Counter();
 
+        //string countBase = 
         public Form1()
         {
             InitializeComponent();
+
         }
 
         public void Form1_Load(object sender, EventArgs e)
+
         {
-
-
-            List<string> listEngwords = new List<string>();
-
-            foreach (var item in File.ReadLines(pathEng))
-            {
-                listEngwords.Add(item);
-            }
-
-            List<string> listRuswords = new List<string>();
-
-            foreach (var item in File.ReadLines(pathRus))
-            {
-                listRuswords.Add(item);
-            }
-
-            label3.Text = listEngwords.Count.ToString();
-            label4.Text = listRuswords.Count.ToString();
-
-
-
+            label3.Text = commonCount.ToString();
+            label4.Text = commonCount.ToString();
+            button1.Enabled = false;
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
 
+            
 
 
             using (StreamWriter addToEngList = new StreamWriter(pathEng, true))
-            {
-                addToEngList.WriteLine(textBox1.Text.ToUpper());
+                {
+                    addToEngList.WriteLine(textBox1.Text.ToUpper());
 
+                    
+                }
+
+                using (StreamWriter addToRusList = new StreamWriter(pathRus, true))
+                {
+                    addToRusList.WriteLine(textBox2.Text.ToUpper());
+
+                    
+                }
+
+                label3.Text = Counter().ToString();
+                label4.Text = Counter().ToString();
                 textBox1.Text = "";
-            }
-
-            using (StreamWriter addToRusList = new StreamWriter(pathRus, true))
-            {
-                addToRusList.WriteLine(textBox2.Text.ToUpper());
-
                 textBox2.Text = "";
-            }
+
 
 
 
         }
 
-       
+        private void button2_Click(object sender, EventArgs e)
+        {
+             
+           
+            
+        }
+
+         
+
+        List<string> BaseOfWords()
+        {
+            
+
+            List<string> listEngwords = new List<string>();
+
+            foreach (var item in File.ReadLines(pathEng))
+
+                listEngwords.Add(item);
+
+            
+
+            List<string> listRuswords = new List<string>();
+
+            foreach (var item in File.ReadLines(pathRus))
+
+                listRuswords.Add(item);
+
+            return listEngwords;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text == "" || textBox2.Text == "")
+                button1.Enabled = false;
+            else
+                button1.Enabled = true;
+            Cheker(BaseOfWords());
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+                button1.Enabled = false;
+            else
+                button1.Enabled = true;
+            
+        }
+
+        void Cheker(List<string> collection)
+        {
+            
+
+            
+            foreach (var item in collection)
+            {
+                if (item == textBox1.Text.ToUpper())
+                    MessageBox.Show("This word already added!");
+
+            }
+
+            
+        }
     }
 }
